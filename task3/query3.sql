70ca1ca2dc400d253dcc2704677730b1ca0c0f34
Adam Světlík <adam.m.svetlik@gmail.com>
2025-01-16 10:52:11 +0100

    query3 alternative solution

--- task3/query3.sql
+++ task3/query3.sql
@@ -3,10 +3,32 @@
 SELECT
     concat(c.first_name, ' ', c.last_name) AS "Full name",
     c.email AS "E-mail",
-    (extract(DAY FROM (coalesce(r.return_date, now()) - r.rental_date)) - f.rental_duration) * f.rental_rate AS "Fine"
+    extract(DAY FROM (coalesce(r.return_date, now()) - r.rental_date)) - f.rental_duration AS "Days overdue",
+    (extract(DAY FROM (coalesce(r.return_date, now()) - r.rental_date)) - f.rental_duration) * f.rental_rate * 0.01 AS "Fine"
 FROM rental r
     LEFT JOIN inventory i ON r.inventory_id = i.inventory_id
     LEFT JOIN film f ON i.film_id = f.film_id
     LEFT JOIN customer c ON r.customer_id = c.customer_id
 WHERE extract(DAY FROM (coalesce(r.return_date, now()) - r.rental_date)) - f.rental_duration > 0;
 -- found that there are fields in film table with rental duration and rental rate, so I decided to use them instead
+
+
+-- could be done with a subquery instead of redundant calculations
+SELECT
+    concat(c.first_name, ' ', c.last_name) AS "Full name",
+    c.email AS "E-mail",
+    overdue_info.days_overdue AS "Days overdue",
+    overdue_info.days_overdue * f.rental_rate * 0.01 AS "Fine"
+FROM rental r
+    LEFT JOIN inventory i ON r.inventory_id = i.inventory_id
+    LEFT JOIN film f ON i.film_id = f.film_id
+    LEFT JOIN customer c ON r.customer_id = c.customer_id
+    JOIN (
+        SELECT
+            r.rental_id,
+            extract(DAY FROM (coalesce(r.return_date, now()) - r.rental_date)) - f.rental_duration AS days_overdue
+        FROM rental r
+        LEFT JOIN inventory i ON r.inventory_id = i.inventory_id
+        LEFT JOIN film f ON i.film_id = f.film_id
+    ) overdue_info ON r.rental_id = overdue_info.rental_id
+WHERE overdue_info.days_overdue > 0;
